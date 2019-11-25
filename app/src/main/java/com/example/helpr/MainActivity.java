2595cf902bf05836af8a01b3a37e78e6a427da84
arnavbatta <arnavb3@illinois.edu>
2019-11-25 15:02:53 -0600

    commit from arnav 2

--- app/src/main/java/com/example/helpr/MainActivity.java
+++ app/src/main/java/com/example/helpr/MainActivity.java
@@ -20,7 +20,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import android.telephony.SmsManager;
-
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
